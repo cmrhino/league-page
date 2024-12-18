2ad77451f43a9f723e1d1b9eff7c16a52b9b37ef
cmrhino <114347443+cmrhino@users.noreply.github.com>
2024-12-18 04:19:38 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1082454522770948096"; // your league ID
-export const leagueName = "Justice League"; // your league name
+export const leagueID = "1123742564093358080"; // your league ID
+export const leagueName = "League of Doom"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
