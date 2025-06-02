49155e7cd72be70a407fc4f0f25d52c863338af6
cmrhino <114347443+cmrhino@users.noreply.github.com>
2025-06-02 02:52:19 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-    // {
+     {
        "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "Chris",
     //   "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
@@ -49,7 +49,7 @@ export const managers = [
     //   "philosophy": "Your fantasy team's philosophy",
     //   "tradingScale": 10, // 1 - 10
     //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    // },
+     },
     // {
     //   "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     //   "name": "Your Name",
